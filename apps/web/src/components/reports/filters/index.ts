f3ff05b85e62a0394bce644b90ffda88d0dd1efb
darioristic <darioristic@gmail.com>
2025-11-29 16:37:49 +0100

    Fix missing exports for status options in reports filters

--- apps/web/src/components/reports/filters/index.ts
+++ apps/web/src/components/reports/filters/index.ts
@@ -2,7 +2,7 @@ export { ReportFilters } from "./report-filters";
 export { DateRangeFilter } from "./date-range-filter";
 export { CompanyFilter } from "./company-filter";
 export { ProjectFilter } from "./project-filter";
-export { StatusFilter } from "./status-filter";
+export { StatusFilter, invoiceStatusOptions, projectStatusOptions } from "./status-filter";
 export { ExportButton } from "./export-button";
 export { useReportFilters } from "./use-report-filters";
 
